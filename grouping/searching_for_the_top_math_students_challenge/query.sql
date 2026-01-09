6086c70e2d4697f91ddf06f3018dc85101732876
NoMoreSey26 <>
2026-01-09 04:38:07 +0000

    Searching for the Top Math Students Challenge

---
+++ grouping/searching_for_the_top_math_students_challenge/query.sql
@@ -0,0 +1,6 @@
+select student_surname, avg(grade) as average_grade
+from student_grades
+where subject_name='Mathematics' and grade>= 90
+group by student_surname
+order by average_grade
+limit 10;
